134f96287adf740639201198f3f36e082616cf98
intern-2 <tudor.farcas2002@gmail.com>
2024-08-22 14:36:31 +0300

    new config for cloud implementation

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    open: true,
+    hmr: {
+      port: 80,
+    },
     proxy: {
       '/auth': {
         target: 'https://auth-dev-1.app.toradev.net',
